518d56781a6a208c59a522d745ab65cd02c2d49e
turkeystar142 <54994475+turkeystar142@users.noreply.github.com>
2026-02-14 18:25:54 -0400

    Delete php files; Update deploy yaml to grab correct filetypes

--- list-articles.php
+++
@@ -1,6 +0,0 @@
-<?php
-$files = glob('articles/*.txt');
-$files = array_map('basename', $files);
-header('Content-Type: application/json');
-echo json_encode($files);
-?>
--- list-images.php
+++
@@ -1,6 +0,0 @@
-<?php
-$files = glob('images/*.{png,jpg,jpeg,gif,webp}', GLOB_BRACE);
-$files = array_map('basename', $files);
-header('Content-Type: application/json');
-echo json_encode($files);
-?>
